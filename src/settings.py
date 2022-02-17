6864dfab98209f04f8a61687c8e0fa6ec23adbf8
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-17 17:50:34 -0500

    4 - Migrate databases and Create a custom User model

--- src/settings.py
+++ src/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+AUTH_USER_MODEL = 'base.User'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
