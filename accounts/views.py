1e995d8b8c446ff3f32f81c2b926c9a6cbf2a603
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-18 17:48:07 -0500

    9 - Setup basic authentication

--- accounts/views.py
+++ accounts/views.py
@@ -4,15 +4,29 @@ from .decorators import (
     unauthenticated_only
 )
 
+from django.contrib.auth.forms import AuthenticationForm
+
+from django.contrib.auth import (
+    login, 
+    logout,
+    # update_session_auth_hash
+)
+
 @unauthenticated_only
 def LoginView(request, *args, **kwargs):
     template = "auth/login.html"
+    form = AuthenticationForm(request, data=request.POST or None)
+    if form.is_valid():
+        user_ = form.get_user()
+        login(request, user_)
+        return redirect("/")
     context = {
-
+       "form" : form
     }
 
     return render(request, template, context)
 
 @login_required
 def LogoutView(request, *args, **kwargs):
-    return redirect('/')
+    logout(request)
+    return redirect('accounts-login')
