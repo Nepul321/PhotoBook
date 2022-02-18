f2b1c5132acdd928cb39dbeed6862db5259771db
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-18 17:58:57 -0500

    11 - Create a Database model for posts

--- posts/models.py
+++ posts/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from base.models import User
+
+class Post(models.Model):
+    image = models.ImageField(upload_to="posted_images/")
+    caption = models.TextField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="post_likes", blank=True)
+
+    class Meta:
+        ordering = ['-datetime']
