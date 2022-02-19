e2b00d75e8ccdaf9376eb522773a83e24abee4cd
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-18 20:39:28 -0500

    16 - Add user permissions to the PostListView

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .serializers import PostSerializer
 from .models import Post
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated, IsAdminUser])
 def PostListView(request, *args, **kwargs):
     context = {'request' : request}
     qs = Post.objects.all()
