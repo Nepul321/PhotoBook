72e0ab809fe170af005bd56f9dd1f6d797c3db23
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-18 20:31:18 -0500

    13 - Setup serializers

---
+++ accounts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from base.models import User
+
+class UserPublicSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=False, allow_blank=True, read_only=True)
+    class Meta:
+        model = User
+        fields = [
+            'username',  
+            'name'
+        ]
---
+++ posts/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import (
+    Post
+)
+
+from accounts.serializers import UserPublicSerializer
+
+class PostSerializer(serializers.Serializer):
+    user = UserPublicSerializer(read_only=True)
+    likes = serializers.SerializerMethodField(read_only=True)
+    is_owner = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Post
+        fields = ('image', 'caption', 'user', 'date', 'likes', 'is_owner')
+
+    def get_likes(self, obj):
+        likes = obj.likes.count()
+        return likes
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        is_owner = False
+        if request.user == obj.user:
+            is_owner = True
+        return is_owner
