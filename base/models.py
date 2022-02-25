dfca887ca34d233bdca2967694eb29732fa41089
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-25 17:44:07 -0500

    28 - Create a profile when ever a user is created

--- base/models.py
+++ base/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+from profiles.models import Profile
 
 class User(AbstractUser):
     name = models.CharField(max_length=255)
@@ -10,3 +14,10 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', ]
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile(sender, instance=None, created=False, **kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from base.models import User
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
