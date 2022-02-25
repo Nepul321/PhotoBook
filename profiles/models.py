7b8b91453dacabd0871556ed70bd14b6aac15ccc
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-25 17:37:04 -0500

    27 - Create a DB model for Profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from base.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    bio = models.TextField(blank=True)
+    profile_pic = models.ImageField(upload_to="profile-pics/", blank=True, null=True)
+    following = models.ManyToManyField(User, related_name="following", blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    joined = models.DateField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_created']
