a05c7991b72bf2f1dac0b5f4cd9591d6a863be59
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-03-27 17:04:44 -0400

    79 - Create a comments model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from posts.models import Post
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-datetime']
+
+    
