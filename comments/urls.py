8af899addc48c8486aaeed20b3701bdd58efd83b
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-03-27 22:34:15 -0400

    82 - Create a Post Comments Thread view

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,7 @@
 from .views import (
     CommentListView,
-    CommentDetailView
+    CommentDetailView,
+    PostCommentsView
 )
 
 from django.urls import path
@@ -8,4 +9,5 @@ from django.urls import path
 urlpatterns = [
     path('', CommentListView, name="comments-list"),
     path('<int:id>/', CommentDetailView, name="comments-detail"),
+    path('posts/<int:id>/', PostCommentsView, name="posts-comments-list")
 ]
--- comments/views.py
+++ comments/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Comment
 from .serializers import CommentSerializer
+from posts.models import Post
+from django.db.models import Q
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated, IsAdminUser])
@@ -24,3 +26,25 @@ def CommentDetailView(request, id, *args, **kwargs):
     serializer = CommentSerializer(obj, context=context)
     data = serializer.data
     return Response(data, status=200)
+
+@api_view(["GET"])
+def PostCommentsView(request, id, *args, **kwargs):
+    context = {"request" : request}
+    qs = Post.objects.filter(
+        Q(id=id),
+        Q(is_private=False)
+    )
+    if not qs:
+        return Response({"detail" : "Post not found"}, status=404)
+    obj = qs.first()
+    commentsFinal = []
+    commentQs = Comment.objects.filter(post=obj)
+    for comment in commentQs:
+        if comment.is_child == False:
+            commentsFinal.append(comment)
+
+        
+    serializer = CommentSerializer(commentsFinal, many=True, context=context)
+    data = serializer.data
+    return Response(data, status=200)
+
