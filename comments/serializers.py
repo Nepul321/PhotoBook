518bce4828927a524f81c40a7d71b40b0f22dee2
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-04-04 22:13:15 -0400

    90 - Add an is_user field to the comment serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -16,14 +16,22 @@ class CommentSerializer(serializers.ModelSerializer):
     user = UserPublicSerializer(read_only=True)
     post = PostSerializer(read_only=True)
     children = serializers.SerializerMethodField(read_only=True)
+    is_user = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ('id', 'user', 'content', 'date', 'datetime', 'post', 'children')
+        fields = ('id', 'user', 'content', 'date', 'datetime', 'post', 'children', 'is_user')
 
     def get_children(self, obj):
         qs = Comment.objects.filter(parent__pk=obj.pk)
         serializer = ChildCommentSeriailzer(
             qs, many=True, context={"request": self.context['request']})
         return serializer.data
+
+    def get_is_user(self, obj):
+        request = self.context.get("request")
+        is_user = False
+        if request.user == obj.user:
+            is_user = True
+        return is_user
 
